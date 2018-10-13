0a1db88e6ff12552bc45dfd8bd2845ce9bb491a3
Nicat17 <nicata099@gmail.com>
2018-10-13 13:58:59 +0400

    RegsiterAdded

--- Blogezy_App/Blogezy_App/Controllers/AccountController.cs
+++ Blogezy_App/Blogezy_App/Controllers/AccountController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Blogezy_App.Data;
 using Blogezy_App.Models;
 using Blogezy_App.Models.ViewModels;
 using Microsoft.AspNetCore.Http;
@@ -15,6 +16,16 @@ namespace Blogezy_App.Controllers
 
         private readonly UserManager<UserApp> _userManager;
         private readonly SignInManager<UserApp> _signInManager;
+        private readonly BlogezyDbContext _blogezyDb;
+
+        public AccountController(UserManager<UserApp> userManager,
+                                    SignInManager<UserApp> signInManager,
+                                        BlogezyDbContext blogezyDb)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _blogezyDb = blogezyDb;
+        }
 
         [HttpGet]
         public IActionResult Login()
@@ -25,16 +36,51 @@ namespace Blogezy_App.Controllers
         [HttpPost]
         public async Task<IActionResult> Login(LoginModel loginModel)
         {
-            UserApp appUser = await _userManager.FindByEmailAsync(loginModel.Email);
+            
             if (ModelState.IsValid)
             {
-                 await _signInManager.PasswordSignInAsync(appUser, loginModel.Password,true, true);
+                await _signInManager.SignOutAsync();
+
+                UserApp appUser = await _userManager.FindByEmailAsync(loginModel.Email);
+
+                if (appUser !=null)
+                {
+                    await _signInManager.PasswordSignInAsync(appUser, loginModel.Password, true, true);
 
                     HttpContext.Session.SetString("name", appUser.UserName);
 
                     HttpContext.Session.SetString("id", appUser.Id);
-                    return RedirectToAction("Index", "Home", new { area = "Admin" });
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            return View();
+        }
 
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterModel registerModel)
+        {
+            if (ModelState.IsValid)
+            {
+                UserApp userApp = new UserApp()
+                {
+                    Name = registerModel.Name,
+                    SurName = registerModel.SurName,
+                    Email = registerModel.Email
+                };
+
+                IdentityResult identityResult = await _userManager.CreateAsync(userApp,registerModel.Password);
+
+                if (identityResult.Succeeded)
+                {
+                    
+                }
             }
             return View();
         }
--- Blogezy_App/Blogezy_App/Models/ViewModels/RegisterModel.cs
+++ Blogezy_App/Blogezy_App/Models/ViewModels/RegisterModel.cs
@@ -19,10 +19,15 @@ namespace Blogezy_App.Models.ViewModels
         [Required]
         public string Email { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Password is required")]
+        [StringLength(255, ErrorMessage = "Must be between 5 and 255 characters", MinimumLength = 5)]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Confirm Password is required")]
+        [StringLength(255, ErrorMessage = "Must be between 5 and 255 characters", MinimumLength = 5)]
+        [DataType(DataType.Password)]
+        [Compare("Password")]
         public string ConfirmPassword { get; set; }
 
     }
