6b344930469f3e8c288eb73eaf5f3ad2985305bb
Nicat17 <nicata099@gmail.com>
2018-10-13 12:52:56 +0400

    Database Psuhed info

--- Blogezy_App/Blogezy_App/Controllers/HomeController.cs
+++ Blogezy_App/Blogezy_App/Controllers/HomeController.cs
@@ -2,15 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Blogezy_App.Data;
+using Blogezy_App.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Blogezy_App.Controllers
 {
     public class HomeController : Controller
     {
+        private BlogezyDbContext _blogezyDb { get; set; }
+
+        public HomeController(BlogezyDbContext blogezyDb)
+        {
+            _blogezyDb = blogezyDb;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            List<Article> articles = _blogezyDb.Articles.ToList();
+
+            return View(articles);
         }
     }
 }
