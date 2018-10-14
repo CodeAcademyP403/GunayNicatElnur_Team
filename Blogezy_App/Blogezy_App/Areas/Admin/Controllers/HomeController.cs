c31ac4f8a2768869cb9e01db1d56b220b1472a49
Nicat17 <nicata099@gmail.com>
2018-10-14 09:50:26 +0400

    Admin Panel is Added

---
+++ Blogezy_App/Blogezy_App/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Blogezy_App.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Blogezy_App.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blogezy_App.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    //[Authorize]
+    public class HomeController : Controller
+    {
+ 
+        [HttpGet]
+        public IActionResult Index()
+        {
+                                       
+            return View();
+        }
+    }
+}
